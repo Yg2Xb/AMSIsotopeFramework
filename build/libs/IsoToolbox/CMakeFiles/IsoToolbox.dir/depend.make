f5ef9a6e7a255b4089ec208b14bb120ab0aec48c
ZixuanYan <zixuan.yan@cern.ch>
2025-08-29 23:23:29 +0200

    Remove build directory from tracking

--- build/libs/IsoToolbox/CMakeFiles/IsoToolbox.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for IsoToolbox.
-# This may be replaced when dependencies are built.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /afs/cern.ch/user/z/zixuan/public/AMSIsotopeFramework/tests
-# Build directory: /afs/cern.ch/user/z/zixuan/public/AMSIsotopeFramework/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LoggerTest "/afs/cern.ch/user/z/zixuan/public/AMSIsotopeFramework/build/tests/runUnitTests")
-set_tests_properties(LoggerTest PROPERTIES  _BACKTRACE_TRIPLES "/afs/cern.ch/user/z/zixuan/public/AMSIsotopeFramework/tests/CMakeLists.txt;16;add_test;/afs/cern.ch/user/z/zixuan/public/AMSIsotopeFramework/tests/CMakeLists.txt;0;")
